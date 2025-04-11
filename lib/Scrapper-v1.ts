0e8bf9d6a5bd67a5bd0c0ea809aac611470e8282
Armin Eslami <armineslami@hotmail.com>
2025-04-11 13:06:58 +0330

    comment out the old scrapper

--- lib/Scrapper-v1.ts
+++ lib/Scrapper-v1.ts
@@ -1,248 +1,248 @@
-import * as cheerio from "cheerio";
-import DivarAdvertise from "@/interface/DivarAdvertise";
-import puppeteer, { ProtocolError } from "puppeteer";
+// import * as cheerio from "cheerio";
+// import DivarAdvertise from "@/interface/DivarAdvertise";
+// import puppeteer, { ProtocolError } from "puppeteer";
 
-class Scrapper {
-  headless: boolean;
+// class Scrapper {
+//   headless: boolean;
 
-  constructor(headless: boolean = true) {
-    this.headless = headless;
-  }
+//   constructor(headless: boolean = true) {
+//     this.headless = headless;
+//   }
 
-  async scrapeDivar(
-    url: string,
-    scrollTimes: number = 0,
-    openLinks: boolean = true
-  ): Promise<DivarAdvertise[]> {
-    const browser = await puppeteer.launch({ headless: this.headless });
-    const page = await browser.newPage();
-    const extractedData: DivarAdvertise[] = [];
+//   async scrapeDivar(
+//     url: string,
+//     scrollTimes: number = 0,
+//     openLinks: boolean = true
+//   ): Promise<DivarAdvertise[]> {
+//     const browser = await puppeteer.launch({ headless: this.headless });
+//     const page = await browser.newPage();
+//     const extractedData: DivarAdvertise[] = [];
 
-    try {
-      await page.goto(url, { waitUntil: "networkidle2" });
-    } catch (error) {
-      console.error("Navigation error:", error);
-      await browser.close();
-      if (error instanceof ProtocolError) {
-        throw new ProtocolError();
-      }
-      return extractedData;
-    }
+//     try {
+//       await page.goto(url, { waitUntil: "networkidle2" });
+//     } catch (error) {
+//       console.error("Navigation error:", error);
+//       await browser.close();
+//       if (error instanceof ProtocolError) {
+//         throw new ProtocolError();
+//       }
+//       return extractedData;
+//     }
 
-    const extractAdvertises = async () => {
-      const html = await page.content();
-      const $ = cheerio.load(html);
-      const articles = $("#post-list-container-id article").toArray();
+//     const extractAdvertises = async () => {
+//       const html = await page.content();
+//       const $ = cheerio.load(html);
+//       const articles = $("#post-list-container-id article").toArray();
 
-      let advertise: DivarAdvertise = {};
+//       let advertise: DivarAdvertise = {};
 
-      // Loop through articles using their index
-      for (let i = 0; i < articles.length; i++) {
-        // articles.length
-        // reset;
-        advertise = {
-          id: crypto.randomUUID(),
-          details: [],
-        };
+//       // Loop through articles using their index
+//       for (let i = 0; i < articles.length; i++) {
+//         // articles.length
+//         // reset;
+//         advertise = {
+//           id: crypto.randomUUID(),
+//           details: [],
+//         };
 
-        //articles.length
-        const article = articles[i];
-        const articleElement = $(article);
+//         //articles.length
+//         const article = articles[i];
+//         const articleElement = $(article);
 
-        // Extract basic listing data
-        const titleElement =
-          articleElement.find(".kt-post-card__title").first() ||
-          articleElement.find(".unsafe-kt-post-card__title").first();
-        const attributeOneElement =
-          articleElement.find(".kt-post-card__description").first() ||
-          articleElement.find(".unsafe-kt-post-card__description").first();
-        const attributeTwoElement =
-          articleElement.find(".kt-post-card__description").eq(1) ||
-          articleElement.find(".unsafe-kt-post-card__description").eq(1);
-        const thumbnailElement =
-          articleElement.find(".kt-post-card-thumbnail img").first() ||
-          articleElement.find(".unsafe-kt-post-card-thumbnail img").first();
+//         // Extract basic listing data
+//         const titleElement =
+//           articleElement.find(".kt-post-card__title").first() ||
+//           articleElement.find(".unsafe-kt-post-card__title").first();
+//         const attributeOneElement =
+//           articleElement.find(".kt-post-card__description").first() ||
+//           articleElement.find(".unsafe-kt-post-card__description").first();
+//         const attributeTwoElement =
+//           articleElement.find(".kt-post-card__description").eq(1) ||
+//           articleElement.find(".unsafe-kt-post-card__description").eq(1);
+//         const thumbnailElement =
+//           articleElement.find(".kt-post-card-thumbnail img").first() ||
+//           articleElement.find(".unsafe-kt-post-card-thumbnail img").first();
 
-        advertise.title = titleElement.text().trim();
-        advertise.attributeOne = attributeOneElement.text().trim();
-        advertise.attributeTwo = attributeTwoElement.text().trim();
-        advertise.thumbnail =
-          thumbnailElement.attr("data-src") ||
-          thumbnailElement.attr("src") ||
-          "";
+//         advertise.title = titleElement.text().trim();
+//         advertise.attributeOne = attributeOneElement.text().trim();
+//         advertise.attributeTwo = attributeTwoElement.text().trim();
+//         advertise.thumbnail =
+//           thumbnailElement.attr("data-src") ||
+//           thumbnailElement.attr("src") ||
+//           "";
 
-        const linkElement =
-          articleElement.find(".kt-post-card__action").first() ||
-          articleElement.find(".unsafe-kt-post-card__action").first();
-        const linkUrl = linkElement.attr("href");
+//         const linkElement =
+//           articleElement.find(".kt-post-card__action").first() ||
+//           articleElement.find(".unsafe-kt-post-card__action").first();
+//         const linkUrl = linkElement.attr("href");
 
-        if (linkUrl && openLinks) {
-          const fullUrl = new URL(linkUrl, new URL(page.url()).origin).href;
-          try {
-            const detailPage = await browser.newPage();
-            await detailPage.goto(fullUrl, { waitUntil: "networkidle2" });
+//         if (linkUrl && openLinks) {
+//           const fullUrl = new URL(linkUrl, new URL(page.url()).origin).href;
+//           try {
+//             const detailPage = await browser.newPage();
+//             await detailPage.goto(fullUrl, { waitUntil: "networkidle2" });
 
-            // Wait for and extract the desired detail data:
-            await detailPage
-              .waitForSelector("article", {
-                timeout: 10000,
-              })
-              .catch(() => {
-                console.warn("Detail data not loaded within timeout.");
-              });
+//             // Wait for and extract the desired detail data:
+//             await detailPage
+//               .waitForSelector("article", {
+//                 timeout: 10000,
+//               })
+//               .catch(() => {
+//                 console.warn("Detail data not loaded within timeout.");
+//               });
 
-            const html = await detailPage.content();
-            const $ = cheerio.load(html);
+//             const html = await detailPage.content();
+//             const $ = cheerio.load(html);
 
-            // Details container
-            const details = $("article div");
+//             // Details container
+//             const details = $("article div");
 
-            // Details section
-            const detailsSection = details
-              .first()
-              .find("section")
-              .eq(0)
-              .children()
-              .last();
+//             // Details section
+//             const detailsSection = details
+//               .first()
+//               .find("section")
+//               .eq(0)
+//               .children()
+//               .last();
 
-            // Description section
-            const descriptionSection = details
-              .first() // first div
-              .find("section")
-              .eq(1); // second section
+//             // Description section
+//             const descriptionSection = details
+//               .first() // first div
+//               .find("section")
+//               .eq(1); // second section
 
-            // Image section
-            const imagesSection = details.eq(0).children().eq(1);
+//             // Image section
+//             const imagesSection = details.eq(0).children().eq(1);
 
-            // Extracting the top details
-            const table = detailsSection.find("table").first();
+//             // Extracting the top details
+//             const table = detailsSection.find("table").first();
 
-            // Get the data from the `thead` (headers)
-            let headers: string[] = [];
-            table.find("thead th").each((i, element) => {
-              headers.push($(element).text().trim());
-            });
+//             // Get the data from the `thead` (headers)
+//             let headers: string[] = [];
+//             table.find("thead th").each((i, element) => {
+//               headers.push($(element).text().trim());
+//             });
 
-            // Get the data from the `tbody` (rows)
-            let rows: string[] = [];
-            table.find("tbody td").each((i, element) => {
-              rows.push($(element).text().trim());
-            });
+//             // Get the data from the `tbody` (rows)
+//             let rows: string[] = [];
+//             table.find("tbody td").each((i, element) => {
+//               rows.push($(element).text().trim());
+//             });
 
-            if (headers.length === rows.length) {
-              for (let i = 0; i < headers.length; i++) {
-                advertise.details?.push({ title: headers[i], value: rows[i] });
-              }
-            }
+//             if (headers.length === rows.length) {
+//               for (let i = 0; i < headers.length; i++) {
+//                 advertise.details?.push({ title: headers[i], value: rows[i] });
+//               }
+//             }
 
-            // Extrating the details
-            const detailsSectionRows = detailsSection
-              .find(".kt-base-row")
-              .toArray();
+//             // Extrating the details
+//             const detailsSectionRows = detailsSection
+//               .find(".kt-base-row")
+//               .toArray();
 
-            // const detailsObject: { title?: string; value?: string } = {};
-            for (let i = 0; i < detailsSectionRows.length; i++) {
-              const row = detailsSectionRows[i];
-              const rowElement = $(row);
-              const title = rowElement.children().first().text().trim();
-              const value = rowElement.children().eq(1).text().trim();
+//             // const detailsObject: { title?: string; value?: string } = {};
+//             for (let i = 0; i < detailsSectionRows.length; i++) {
+//               const row = detailsSectionRows[i];
+//               const rowElement = $(row);
+//               const title = rowElement.children().first().text().trim();
+//               const value = rowElement.children().eq(1).text().trim();
 
-              if (title && value) {
-                advertise.details?.push({
-                  title: title,
-                  value: value,
-                });
-              }
-            }
+//               if (title && value) {
+//                 advertise.details?.push({
+//                   title: title,
+//                   value: value,
+//                 });
+//               }
+//             }
 
-            // Extracting the description
-            advertise.description = descriptionSection
-              .children()
-              .find(".kt-description-row__text")
-              .text();
+//             // Extracting the description
+//             advertise.description = descriptionSection
+//               .children()
+//               .find(".kt-description-row__text")
+//               .text();
 
-            advertise.image = imagesSection
-              .first()
-              .find(".keen-slider img")
-              ?.attr("src");
+//             advertise.image = imagesSection
+//               .first()
+//               .find(".keen-slider img")
+//               ?.attr("src");
 
-            // The second table is attributes which exists for some advertises
-            const attributesTable = detailsSection.find("table").eq(1);
+//             // The second table is attributes which exists for some advertises
+//             const attributesTable = detailsSection.find("table").eq(1);
 
-            // Get the data from the `thead` (headers)
-            headers = [];
-            attributesTable.find("thead th").each((i, element) => {
-              headers.push($(element).text().trim());
-            });
+//             // Get the data from the `thead` (headers)
+//             headers = [];
+//             attributesTable.find("thead th").each((i, element) => {
+//               headers.push($(element).text().trim());
+//             });
 
-            // Get the data from the `tbody` (rows)
-            rows = [];
-            attributesTable.find("tbody td").each((i, element) => {
-              rows.push($(element).text().trim());
-            });
+//             // Get the data from the `tbody` (rows)
+//             rows = [];
+//             attributesTable.find("tbody td").each((i, element) => {
+//               rows.push($(element).text().trim());
+//             });
 
-            if (headers.length === rows.length) {
-              for (let i = 0; i < headers.length; i++) {
-                advertise.details?.push({ title: "امکانات", value: rows[i] });
-              }
-            }
+//             if (headers.length === rows.length) {
+//               for (let i = 0; i < headers.length; i++) {
+//                 advertise.details?.push({ title: "امکانات", value: rows[i] });
+//               }
+//             }
 
-            await detailPage.close();
-          } catch (detailError) {
-            console.error(
-              `Error processing detail page for ${fullUrl}:`,
-              detailError
-            );
-          }
-        }
+//             await detailPage.close();
+//           } catch (detailError) {
+//             console.error(
+//               `Error processing detail page for ${fullUrl}:`,
+//               detailError
+//             );
+//           }
+//         }
 
-        // Avoid duplicates
-        if (
-          advertise.title &&
-          !extractedData.find(
-            (c) =>
-              c.title === advertise.title &&
-              c.attributeTwo === advertise.attributeTwo &&
-              c.thumbnail === advertise.thumbnail
-          )
-        ) {
-          extractedData.push(advertise);
-        }
-      }
-    };
+//         // Avoid duplicates
+//         if (
+//           advertise.title &&
+//           !extractedData.find(
+//             (c) =>
+//               c.title === advertise.title &&
+//               c.attributeTwo === advertise.attributeTwo &&
+//               c.thumbnail === advertise.thumbnail
+//           )
+//         ) {
+//           extractedData.push(advertise);
+//         }
+//       }
+//     };
 
-    // **Step 1: Scrape initial content**
-    await extractAdvertises();
+//     // **Step 1: Scrape initial content**
+//     await extractAdvertises();
 
-    // **Step 2: Scroll and load more content**
-    for (let i = 0; i < scrollTimes; i++) {
-      await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+//     // **Step 2: Scroll and load more content**
+//     for (let i = 0; i < scrollTimes; i++) {
+//       await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+//       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      const loadMoreButton = await page.$(
-        ".post-list__load-more-btn-container-cef96 button"
-      );
-      if (loadMoreButton) {
-        console.log("Clicking 'Load More' button...");
-        await loadMoreButton.click();
-        await page
-          .waitForSelector("#post-list-container-id article", { timeout: 5000 })
-          .catch(() => {
-            console.warn("No new content loaded after clicking 'Load More'.");
-          });
-      }
+//       const loadMoreButton = await page.$(
+//         ".post-list__load-more-btn-container-cef96 button"
+//       );
+//       if (loadMoreButton) {
+//         console.log("Clicking 'Load More' button...");
+//         await loadMoreButton.click();
+//         await page
+//           .waitForSelector("#post-list-container-id article", { timeout: 5000 })
+//           .catch(() => {
+//             console.warn("No new content loaded after clicking 'Load More'.");
+//           });
+//       }
 
-      await extractAdvertises();
-    }
+//       await extractAdvertises();
+//     }
 
-    await browser.close();
+//     await browser.close();
 
-    console.log("--------Result--------");
-    console.log(extractedData);
-    console.log("----------------------");
+//     console.log("--------Result--------");
+//     console.log(extractedData);
+//     console.log("----------------------");
 
-    return extractedData;
-  }
-}
+//     return extractedData;
+//   }
+// }
 
-export default Scrapper;
+// export default Scrapper;
